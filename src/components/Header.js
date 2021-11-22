c16c321cc65c384dd087436d23d05d39a8f06d5b
Emily Dorgan <wmdorgan@gmail.com>
2021-11-22 03:58:58 -0800

    set up the nav links to conditionally render the different page components

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,12 @@ export default function Header({ currentPage, handlePageChange }){
     return(
         <header>
             <div>
-                <a href="#home"><h1>Emily Dorgan</h1></a>
-                <a href="#home"><div>img placeholder</div></a>
+                <a href="#home" onClick={() => handlePageChange('Home')}>
+                    <h1>Emily Dorgan</h1>
+                </a>
+                <a href="#home" onClick={() => handlePageChange('Home')}>
+                    <div>img placeholder</div>
+                </a>
                 <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
             </div>
         </header>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,38 @@ export default function Nav({ currentPage, handlePageChange }){
     return(
         <nav>
             <div>
+                <ul>
+                    <li>
+                        <a 
+                            href = "#portfolio" 
+                            onClick={() => handlePageChange('Portfolio')}
+                            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                            href = "#contact" 
+                            onClick={() => handlePageChange('Contact')}
+                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Contact Me
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                            href = "#about" 
+                            onClick={() => handlePageChange('About')}
+                            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                        >
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                            Resume
+                    </li>
+                </ul>
             </div>
         </nav>
     )
