4d9fd5cc6517f87c6c25d06c980f1cea1cb5c01f
Emily Dorgan <wmdorgan@gmail.com>
2021-11-22 18:48:09 -0800

    fixed the resume link, added the classes and styling for the nav bar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Nav from './Nav';
+import profilePic from './images/profile.jpg'
 
 export default function Header({ currentPage, handlePageChange }){
     return(
         <header className="navbar navbar-expand-lg text-white fixed-top nav-custom">
-            <div>
-                <a href="#home" onClick={() => handlePageChange('Home')}>
+            <div className="container-fluid">
+                <a className="navbar-brand my-name" href="#home" onClick={() => handlePageChange('Home')}>
                     <h1>Emily Dorgan</h1>
                 </a>
                 <a href="#home" onClick={() => handlePageChange('Home')}>
-                    <div>img placeholder</div>
+                    <img className="profile" src={profilePic} alt="profile" />
                 </a>
                 <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
             </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,41 @@
 import React from "react";
+import resume from './pdf/emd-resume.pdf'
 
 export default function Nav({ currentPage, handlePageChange }){
     return(
-        <nav>
-            <div>
-                <ul>
-                    <li>
-                        <a 
-                            href = "#portfolio" 
-                            onClick={() => handlePageChange('Portfolio')}
-                            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                        >
-                            Projects
-                        </a>
-                    </li>
-                    <li>
-                        <a 
-                            href = "#contact" 
-                            onClick={() => handlePageChange('Contact')}
-                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                        >
-                            Contact Me
-                        </a>
-                    </li>
-                    <li>
-                        <a 
-                            href = "#about" 
-                            onClick={() => handlePageChange('About')}
-                            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                        >
-                            About Me
-                        </a>
-                    </li>
-                    <li>
-                            Resume
-                    </li>
-                </ul>
-            </div>
+        <nav className="collapse navbar-collapse custom-align" id="navbarNav">
+            <ul className="navbar-nav custom-align">
+                <li className="nav-item p-1">
+                    <a 
+                        href = "#portfolio" 
+                        onClick={() => handlePageChange('Portfolio')}
+                        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Projects
+                    </a>
+                </li>
+                <li className="nav-item p-1">
+                    <a 
+                        href = "#contact" 
+                        onClick={() => handlePageChange('Contact')}
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Contact Me
+                    </a>
+                </li>
+                <li className="nav-item p-1">
+                    <a 
+                        href = "#about" 
+                        onClick={() => handlePageChange('About')}
+                        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                    >
+                        About Me
+                    </a>
+                </li>
+                <li className="nav-item p-1">
+                    <a className="nav-link" href ={resume} target="_blank" rel="noreferrer">Resume</a>
+                </li>
+            </ul>
         </nav>
     )
 }
