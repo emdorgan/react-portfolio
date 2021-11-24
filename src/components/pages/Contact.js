cdcef6256ad89608d13b39482ee4fbe4added03a
Emily Dorgan <wmdorgan@gmail.com>
2021-11-23 16:08:28 -0800

    set up the contact form with user validation

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,48 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {validateEmail} from '../../utils/helpers'
 import {FaGithub} from 'react-icons/fa';
 import {FaEnvelope} from 'react-icons/fa';
 import {FaLinkedin} from 'react-icons/fa';
 import {FaTwitter} from 'react-icons/fa';
 
 export default function Contact(){
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [text, setText] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name'){
+            setName(inputValue);
+        } else {
+            setText(inputValue);
+        }
+      };
+
+      const handleFormSubmit = (e) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+    
+        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+        if (!validateEmail(email) || !name) {
+          setErrorMessage('Email or name is invalid');
+          return;
+        }
+    
+        // If everything goes according to plan, we want to clear out the input after a successful registration.
+        setName('');
+        setEmail('');
+        setText('');
+      };
+
     return(
         <section id="contact-me" className="container-fluid">
             <h2>Contact Me</h2>
@@ -18,6 +55,37 @@ export default function Contact(){
                     <li>Instagram: <a href="https://twitter.com/PrezMoocow"><FaTwitter/> https://twitter.com/PrezMoocow</a></li>
                 </ul>
             </article>
+            <div>
+                <form className="form">
+                    <input
+                        value={email}
+                        name="email"
+                        onChange={handleInputChange}
+                        type="email"
+                        placeholder="email"
+                    />
+                    <input
+                        value={name}
+                        name="name"
+                        onChange={handleInputChange}
+                        type="name"
+                        placeholder="name"
+                    />
+                    <input
+                        value={text}
+                        name="text"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="text"
+                    />
+                    <button type="button" onClick={handleFormSubmit}>Submit</button>
+                </form>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+            </div>
         </section>
     )
 }
