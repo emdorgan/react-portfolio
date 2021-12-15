3323b38858909180c5b7f9216b3c61ab7d8eda50
Emily Dorgan <wmdorgan@gmail.com>
2021-12-15 15:35:38 -0800

    re-wrote the text section of the about me, made some styling changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,12 +12,17 @@ export default function About(){
                 <div className="picture-frame">
                     <img src={emily2} alt="Emily1"/>
                 </div>
+                <div className="me-text">
                 <p>
-                I am a Full Stack Web Developer.
-                I've always been a curious person and I have a strong love of learning new things, which is what brought me to the field of web development. Although my professional path has
-          taken some wild turns - most notably with the discovery that I am trans - I am now on track to pursue my dream career of developing attractive and interactive websites. I am currently looking for work
-          after having completed the Full Stack Web Development program at UC Berkeley.
+                I am a Full Stack Web Developer with a background in design and a lot of experience in education. 
+                On the front end, I know HTML, CSS, JavaScript, React, Handlebars, Bootstrap, JQuery and AJAX.
+                On the back end, I know Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose and GraphQL.
+                I also have some UI/UX design experience to complement my technical proficiencies.
                 </p>
+                <p>I've always been a curious person and I have a strong love of learning new things, 
+                which is what brought me to the field of web development. I currently live in the bay area with my cat
+                and I am currently seeking employment as a Full Stack Web Developer or Technical Solutions Engineer.</p>
+                </div>
             </article>
         </section>
     )
