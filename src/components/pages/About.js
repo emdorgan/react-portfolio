4140f6665f087045624abd80a68e0951463b8fd4
Emily Dorgan <wmdorgan@gmail.com>
2021-12-15 15:14:14 -0800

    changed the css and the images in the about me section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,22 +9,15 @@ export default function About(){
         <section id="about-me" className="container-fluid">
             <h2>About Me</h2>
             <article className="about-me">
+                <div className="picture-frame">
+                    <img src={emily2} alt="Emily1"/>
+                </div>
                 <p>
+                I am a Full Stack Web Developer.
                 I've always been a curious person and I have a strong love of learning new things, which is what brought me to the field of web development. Although my professional path has
           taken some wild turns - most notably with the discovery that I am trans - I am now on track to pursue my dream career of developing attractive and interactive websites. I am currently looking for work
           after having completed the Full Stack Web Development program at UC Berkeley.
                 </p>
-                <div className="picture-album">
-                    <div className="picture-frame">
-                        <img src={emily1} alt="Emily1"/>
-                    </div>
-                    <div className="picture-frame">
-                        <img src={emily2} alt="Emily1"/>
-                    </div>
-                    <div className="picture-frame">
-                        <img src={emily3} alt="Emily1"/>
-                    </div>
-                </div>
             </article>
         </section>
     )
